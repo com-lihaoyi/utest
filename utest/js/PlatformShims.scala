15813ea5c1c8dbd0141a497c62ad1d0f49d91595
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-04-11 13:14:10 +0200

    Remove useless export of `PlatformShims`.

--- utest/js/PlatformShims.scala
+++ utest/js/PlatformShims.scala
@@ -2,15 +2,10 @@ package utest
 
 
 import scala.concurrent.Future
-import scala.scalajs.js
-import scala.scalajs.js.annotation.{JSExport, JSExportDescendentObjects}
-import scala.scalajs.runtime.StackTrace.ColumnStackTraceElement
-import scala.util.{Failure, Success}
 
 /**
  * Platform specific stuff that differs between JVM and JS
  */
-@JSExport
 object PlatformShims {
   def await[T](f: Future[T]): T = {
     f.value match {
