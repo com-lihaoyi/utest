b2e75316336468f7cc5da4e0da36ff03b83595df
Li Haoyi <haoyi.sg@gmail.com>
2017-09-22 13:15:17 +0800

    fork readme into readme-dev to allow merging patches without adding unreleased features to the docs; delete commented out code in RetryTests

--- utest/shared/src/test/scala/test/utest/RetryTests.scala
+++ utest/shared/src/test/scala/test/utest/RetryTests.scala
@@ -66,32 +66,3 @@ object LocalRetryTests extends utest.TestSuite{
   }
 }
 
-// TODO: bug with retry assert
-// object LocalRetryBeforeEachTests extends utest.TestSuite{
-//   var x = 0
-//   override def utestBeforeEach() = {
-//     println(s">>> before each x: $x")
-//     x = 0
-//   }
-//   val flaky = new FlakyThing
-
-//   def tests = Tests{
-//     'hello - retry(3){
-//       println(s">>> x: $x")
-//       assert(x == 0)
-//       x += 1
-//       flaky.run
-//     }
-//   }
-// }
-
-// -------------------------------- Running Tests --------------------------------
-//   Setting up CustomFramework
-//   >>> before each x: 0
-//   >>> x: 0
-//   >>> x: 1
-//   >>> x: 1
-//   >>> x: 1
-// X test.utest.LocalRetryBeforeEachTests.hello 20ms
-// utest.AssertionError: assert(x == 0)
-// utest.asserts.Asserts$.assertImpl(Asserts.scala:114)
