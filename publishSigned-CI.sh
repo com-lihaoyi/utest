da71da7c58e1146f0f659baf2a1c8c182d5edec8
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-12-26 18:08:47 +0000

    Add missing space in bash script.

--- publishSigned-CI.sh
+++ publishSigned-CI.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ "$TRAVIS_SECURE_ENV_VARS" == true && "$CI_PUBLISH" == true && -n "$TRAVIS_TAG"]]; then
+if [[ "$TRAVIS_SECURE_ENV_VARS" == true && "$CI_PUBLISH" == true && -n "$TRAVIS_TAG" ]]; then
   echo "Publishing to sonatype..."
   git log | head -n 20
   echo "$PGP_SECRET" | base64 --decode | gpg --import
