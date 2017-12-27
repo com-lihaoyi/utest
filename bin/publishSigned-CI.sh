c324e2367ba8815e13dc789db7c445e9f2097fc8
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-12-27 15:58:37 +0000

    Move scripts to bin/ folder.
    The top-level directory was getting polluted with bash scripts.

--- bin/publishSigned-CI.sh
+++ bin/publishSigned-CI.sh
@@ -3,7 +3,7 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == true && "$CI_PUBLISH" == true && -n "$TRAVIS_
   echo "Publishing to sonatype..."
   git log | head -n 20
   echo "$PGP_SECRET" | base64 --decode | gpg --import
-  ./publishSigned.sh
+  ./bin/publishSigned.sh
 else
   echo "Skipping publish, branch=$TRAVIS_BRANCH"
 fi
