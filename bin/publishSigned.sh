691b6cb3d8acc4b31e3ae70bfd3b06e87fb19472
Nadav Samet <thesamet@gmail.com>
2018-08-22 09:42:39 -0700

    Upgrade to Scala.js 1.0.0-M5

--- bin/publishSigned.sh
+++ bin/publishSigned.sh
@@ -13,6 +13,6 @@ sbt ++2.13.0-M2 \
     utestJS/publishSigned \
     utestJVM/publishSigned
 
-SCALAJS_VERSION=1.0.0-M3 sbt ++2.11.12 utestJS/publishSigned
-SCALAJS_VERSION=1.0.0-M3 sbt ++2.12.4 utestJS/publishSigned
-SCALAJS_VERSION=1.0.0-M3 sbt ++2.13.0-M2 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M5 sbt ++2.11.12 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M5 sbt ++2.12.4 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M5 sbt ++2.13.0-M2 utestJS/publishSigned
