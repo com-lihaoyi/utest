85a86b2b46897ac971e4ea3196dd8d3dbe47a0ba
Sébastien Doeraene <sjrdoeraene@gmail.com>
2018-09-14 13:38:17 +0200

    Upgrade to Scala.js 1.0.0-M5.

--- bin/publishSigned.sh
+++ bin/publishSigned.sh
@@ -13,6 +13,6 @@ sbt ++2.13.0-M3 \
     utestJS/publishSigned \
     utestJVM/publishSigned
 
-SCALAJS_VERSION=1.0.0-M3 sbt ++2.11.12 utestJS/publishSigned
-SCALAJS_VERSION=1.0.0-M3 sbt ++2.12.6 utestJS/publishSigned
-SCALAJS_VERSION=1.0.0-M3 sbt ++2.13.0-M3 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M5 sbt ++2.11.12 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M5 sbt ++2.12.6 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M5 sbt ++2.13.0-M3 utestJS/publishSigned
