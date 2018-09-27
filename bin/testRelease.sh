7253d53ad14323d5ab35f36f5d55d252e92140aa
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-27 11:54:18 +0200

    Update test release script to run resolution in one go

--- bin/testRelease.sh
+++ bin/testRelease.sh
@@ -2,17 +2,19 @@
 set -eux
 
 VERSION=$1
-coursier fetch com.lihaoyi:utest_2.10:$VERSION
-coursier fetch com.lihaoyi:utest_2.11:$VERSION
-coursier fetch com.lihaoyi:utest_2.12:$VERSION
-coursier fetch com.lihaoyi:utest_2.13.0-M5:$VERSION
-coursier fetch com.lihaoyi:utest_2.13.0-M5:$VERSION
-coursier fetch com.lihaoyi:utest_native0.3_2.11:$VERSION
-coursier fetch com.lihaoyi:utest_sjs0.6_2.10:$VERSION
-coursier fetch com.lihaoyi:utest_sjs0.6_2.11:$VERSION
-coursier fetch com.lihaoyi:utest_sjs0.6_2.12:$VERSION
-coursier fetch com.lihaoyi:utest_sjs0.6_2.13.0-M5:$VERSION
-coursier fetch com.lihaoyi:utest_sjs0.6_2.13.0-M5:$VERSION
-coursier fetch com.lihaoyi:utest_sjs1.0.0-M5_2.11:$VERSION
-coursier fetch com.lihaoyi:utest_sjs1.0.0-M5_2.12:$VERSION
-coursier fetch com.lihaoyi:utest_sjs1.0.0-M5_2.13.0-M5:$VERSION
+coursier fetch \
+  com.lihaoyi:utest_2.10:$VERSION \
+  com.lihaoyi:utest_2.11:$VERSION \
+  com.lihaoyi:utest_2.12:$VERSION \
+  com.lihaoyi:utest_2.13.0-M5:$VERSION \
+  com.lihaoyi:utest_2.13.0-M5:$VERSION \
+  com.lihaoyi:utest_native0.3_2.11:$VERSION \
+  com.lihaoyi:utest_sjs0.6_2.10:$VERSION \
+  com.lihaoyi:utest_sjs0.6_2.11:$VERSION \
+  com.lihaoyi:utest_sjs0.6_2.12:$VERSION \
+  com.lihaoyi:utest_sjs0.6_2.13.0-M5:$VERSION \
+  com.lihaoyi:utest_sjs0.6_2.13.0-M5:$VERSION \
+  com.lihaoyi:utest_sjs1.0.0-M5_2.11:$VERSION \
+  com.lihaoyi:utest_sjs1.0.0-M5_2.12:$VERSION \
+  -r sonatype:releases
+  # com.lihaoyi:utest_sjs1.0.0-M5_2.13.0-M5:$VERSION \ # Did not release
