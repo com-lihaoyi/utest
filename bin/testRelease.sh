271c171a70bad0b3c5ffff08255bd7f2623316b0
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-04-07 16:22:31 +0200

    Upgrade to Scala.js 1.0.0-M7 from M5.
    This was needed to cross-build 2.13 on Scala.js 1.0

--- bin/testRelease.sh
+++ bin/testRelease.sh
@@ -14,7 +14,7 @@ coursier fetch \
   com.lihaoyi:utest_sjs0.6_2.12:$VERSION \
   com.lihaoyi:utest_sjs0.6_2.13.0-RC1:$VERSION \
   com.lihaoyi:utest_sjs0.6_2.13.0-RC1:$VERSION \
-  com.lihaoyi:utest_sjs1.0.0-RC1_2.11:$VERSION \
-  com.lihaoyi:utest_sjs1.0.0-RC1_2.12:$VERSION \
+  com.lihaoyi:utest_sjs1.0.0-M7_2.11:$VERSION \
+  com.lihaoyi:utest_sjs1.0.0-M7_2.12:$VERSION \
+  com.lihaoyi:utest_sjs1.0.0-M7_2.13.0-RC1:$VERSION \
   -r sonatype:releases
-  # com.lihaoyi:utest_sjs1.0.0-RC1_2.13.0-RC1:$VERSION \ # Did not release
