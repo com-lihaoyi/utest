7611c8a8dbc29f6a2d0b9768e147916710929e8a
Ross A. Baker <ross@rossabaker.com>
2019-05-24 11:22:11 -0400

    Scala-2.13.0-RC2 support

--- bin/publishSigned.sh
+++ bin/publishSigned.sh
@@ -9,10 +9,10 @@ sbt ++2.11.12 \
 sbt ++2.12.8 \
     utestJS/publishSigned \
     utestJVM/publishSigned
-sbt ++2.13.0-RC1 \
+sbt ++2.13.0-RC2 \
     utestJS/publishSigned \
     utestJVM/publishSigned
 
 SCALAJS_VERSION=1.0.0-M7 sbt ++2.11.12 utestJS/publishSigned
 SCALAJS_VERSION=1.0.0-M7 sbt ++2.12.8 utestJS/publishSigned
-SCALAJS_VERSION=1.0.0-M7 sbt ++2.13.0-RC1 utestJS/publishSigned
+SCALAJS_VERSION=1.0.0-M7 sbt ++2.13.0-RC2 utestJS/publishSigned
