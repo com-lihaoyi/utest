2fc82f0763f6b35aa771f8762b6bacce3080f963
Anatolii <anatoliykmetyuk@gmail.com>
2019-12-11 17:25:04 +0100

    Share FutureCrashTest between Scala 2 and Scala 3

--- utest/test/src/test/utest/FutureCrashTest.scala
+++ utest/test/src/test/utest/FutureCrashTest.scala
@@ -1,14 +1,14 @@
 package test.utest
 import utest._
-import concurrent.Future
+import concurrent.{Future, ExecutionContext}
 
 object FutureCrashTest extends TestSuite {
   def wrapping[T](f: => T):T = {
     f
   }
-  implicit val queue = utest.framework.ExecutionContext.RunNow
+  implicit val queue: ExecutionContext = utest.framework.ExecutionContext.RunNow
 
-  def tests = TestSuite {
+  def tests = Tests {
     "Crash the compiler when I spew a future" - {
       wrapping { val fut = Future { 1 } }
       // println("This prevents the crash")
