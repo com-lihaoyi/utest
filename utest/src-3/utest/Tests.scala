9f8de2ae2a852611f3a0596472f5fe17d5f6aba5
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-02-04 15:27:32 +0100

    Add workaround for upickle tests

--- utest/src-3/utest/Tests.scala
+++ utest/src-3/utest/Tests.scala
@@ -7,7 +7,14 @@ import scala.collection.mutable
 
 trait TestsVersionSpecific {
   import TestsVersionSpecific._
-  inline def apply(inline expr: Unit): Tests = ${testsImpl('expr)}
+  // FIXME remove transparent
+  //       This there seems to be an issue with case classes in inline defs
+  //       [error] -- Error: /__w/dotty/dotty/community-build/community-projects/upickle/upickle/test/src/upickle/example/ExampleTests.scala:248:19
+  //       [error] 248 |        case class Wrap(i: Int)
+  //       [error]     |                   ^
+  //       [error]     |While expanding a macro, a reference to value i was used outside the scope where it was defined
+  //       [error]     | This location contains code that was inlined from ExampleTests.scala:248
+  transparent inline def apply(inline expr: Unit): Tests = ${testsImpl('expr)}
 }
 
 object TestsVersionSpecific {
