caa7acbcd5286c7fba7b5970abd5e8b344b3691e
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-03-03 14:00:47 +0100

    Cleanup utest macro implementation

--- utest/src-3/utest/Tests.scala
+++ utest/src-3/utest/Tests.scala
@@ -6,7 +6,6 @@ import utest.framework.{TestCallTree, Tree => UTree }
 import scala.collection.mutable
 
 trait TestsVersionSpecific {
-  import TestsVersionSpecific._
   // FIXME remove transparent
   //       This there seems to be an issue with case classes in inline defs
   //       [error] -- Error: /__w/dotty/dotty/community-build/community-projects/upickle/upickle/test/src/upickle/example/ExampleTests.scala:248:19
@@ -14,13 +13,6 @@ trait TestsVersionSpecific {
   //       [error]     |                   ^
   //       [error]     |While expanding a macro, a reference to value i was used outside the scope where it was defined
   //       [error]     | This location contains code that was inlined from ExampleTests.scala:248
-  transparent inline def apply(inline expr: Unit): Tests = ${testsImpl('expr)}
-}
-
-object TestsVersionSpecific {
-  def testsImpl(body: Expr[Any])(using Quotes): Expr[Tests] = {
-    import quotes.reflect._
-    val helpers = new TestBuilder(quotes)
-    helpers.processTests(body.asTerm)
-  }
+  transparent inline def apply(inline expr: Unit): Tests =
+    ${ TestBuilder.processTests('expr) }
 }
