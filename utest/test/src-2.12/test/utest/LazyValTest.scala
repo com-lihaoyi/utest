54700315bf0b2a94dd3bbd47d30728787f50ab0f
Li Haoyi <haoyi.sg@gmail.com>
2025-07-27 21:37:08 +0800

    .

--- utest/test/src-2.12/test/utest/LazyValTest.scala
+++ utest/test/src-2.12/test/utest/LazyValTest.scala
@@ -10,15 +10,15 @@ object LazyValTest extends TestSuite {
   override def tests = TestSuite {
     import What._
     lazy val subj = imported
-    'test{
+    test("test"){
       lazy val y = "hello"
-      'inner{
+      test("inner"){
         lazy val z = "world"
         val res = y * imported + z
         assert(res == "hellohelloworld")
         res
       }
-      'inner2{
+      test("inner2"){
         lazy val terminalLazyVal = y
       }
     }
