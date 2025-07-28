05f29eb8f6d0a7aa6332989011cd3f12b5cd65b3
Li Haoyi <haoyi.sg@gmail.com>
2025-07-28 10:59:58 +0800

    flesh out asserts

--- utest/src/utest/asserts/Asserts.scala
+++ utest/src/utest/asserts/Asserts.scala
@@ -100,18 +100,39 @@ trait Asserts extends AssertsVersionSpecific {
     }
   }
 
-  def assertGoldenFile(path: java.nio.file.Path, text: String): Unit = {
-    val fileContents = java.nio.file.Files.readString(path)
-    if (fileContents != text) {
-      if (!sys.env.contains("UTEST_UPDATE_GOLDEN_TESTS")) Predef.assert(fileContents != text)
-      else GoldenFix.register.value.apply(GoldenFix(path, fileContents, 0, fileContents.length))
+  def assertGoldenFile(path: java.nio.file.Path, testValue: String): Unit = {
+    val goldenFileContents = java.nio.file.Files.readString(path)
+    if (goldenFileContents != testValue) {
+      if (!sys.env.contains("UTEST_UPDATE_GOLDEN_TESTS")) {
+        throw new AssertionError(
+          "Value does not match golden file contents: " + path,
+          Seq(
+            TestValue.Equality(
+              TestValue.Single("goldenFileContents", None, goldenFileContents),
+              TestValue.Single("testValue", None, testValue),
+            )
+          )
+        )
+
+      }
+      else GoldenFix.register.value.apply(GoldenFix(path, goldenFileContents, 0, goldenFileContents.length))
     }
   }
-  def assertGoldenLiteral(value: Any, golden: SourceSpan[Any]): Unit = {
+
+  def assertGoldenLiteral(testValue: Any, golden: SourceSpan[Any]): Unit = {
     val goldenValue = golden.value
-    if (value != goldenValue) {
-      if (!sys.env.contains("UTEST_UPDATE_GOLDEN")) Predef.assert(value == goldenValue)
-      else {
+    if (testValue != goldenValue) {
+      if (!sys.env.contains("UTEST_UPDATE_GOLDEN")) {
+        throw new AssertionError(
+          "Value does not match golden literal contents",
+          Seq(
+            TestValue.Equality(
+              TestValue.Single("testValue", None, testValue),
+              TestValue.Single("goldenValue", None, goldenValue),
+            )
+          )
+        )
+      } else {
        GoldenFix.register.value.apply(
          GoldenFix(
            java.nio.file.Path.of(golden.sourceFile),
